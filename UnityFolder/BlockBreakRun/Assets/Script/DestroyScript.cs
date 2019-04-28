57f05439ab556517b1d4ce5fdda9c207f8ea9e6f
pippi0417 <pippi9400@gmail.com>
2019-04-28 16:28:51 +0900

    First,Commit!!!!!!!!

---
+++ UnityFolder/BlockBreakRun/Assets/Script/DestroyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScript : MonoBehaviour {
+    public float reachableDistance = 100.0f;
+    public Camera playerCamera;
+
+    Ray ray;
+    RaycastHit hitInfo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = playerCamera.ViewportPointToRay(new Vector3(0.0f, 0.0f, 0));
+        bool isRayhit = Physics.Raycast(ray, out hitInfo, reachableDistance);
+        if (isRayhit && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(hitInfo.transform.name);
+        }
+    }
+}
